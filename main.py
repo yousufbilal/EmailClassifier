c8fda0d9a43492bb8cb6d925b8c21a4e387ddfd8
yousufbilal <64967817+yousufbilal@users.noreply.github.com>
2025-12-08 04:28:14 +0000

    implemented

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 import pandas as pd
-
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv('email_dataset.csv')
-## dataframe 
-print(df.text)
+df["clean_text"]=(
+    df["text"]
+   .str.lower()
+    .str.replace("\n", " ", regex=False)
+    .str.replace("\r", " ", regex=False)
+    .str.strip()
+)
+
+texts= df["clean_text"]
+label= df["label"]
+
+vectorizer = TfidfVectorizer(stop_words="english")
+X = vectorizer.fit_transform(texts)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, label, test_size=0.2, random_state=42
+)
+
